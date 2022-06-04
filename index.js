23486c3c1c3bcbe4e4926dc171071ff1b4221741
David Moreno <cesardaviddev@gmail.com>
2022-06-04 21:06:01 +0200

    ENHACEMENT: Making a new function

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
-    }
-    else{
-        document.querySelector("#tasks").innerHTML += `
+// clickear prioridad->border color: different y al añdir new task tambien
+// funcion que en click se añanda todo
+
+document.querySelector();
+document.querySelector();
+document.querySelector();
+
+
+//a Add and delte task
+
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("Please Enter a Task");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
             <div class="task">
                 <span id="taskname">
                     ${document.querySelector("#newtask input").value}
@@ -14,15 +23,11 @@ document.querySelector('#push').onclick = function(){
             </div>
         `;
 
-        const current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-
-      
-
- 
+    const current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
     }
-}
+  }
+};
